b8428f7999fa528f513614fb1d0d213dbb745e1a
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-09-29 23:38:05 +0300

    navigation_screen refactored

--- lib/src/features/navigation/presentation/screens/navigation_screen.dart
+++ lib/src/features/navigation/presentation/screens/navigation_screen.dart
@@ -9,6 +9,16 @@ import '../widgets/navigation_widget.dart';
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
 
+  List<Widget> _buildScreens(AuthState authState) {
+    return [
+      const HomeScreen(),
+      const Text('Search Screen'),
+      const Text('Chat Screen'),
+      const Text('Notification Screen'),
+      authState is Authenticated ? const ProfileScreen() : SignInScreen(),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -20,15 +30,7 @@ class NavigationScreen extends StatelessWidget {
         builder: (context, authState) {
           return BlocBuilder<NavigationBloc, int>(
             builder: (context, selectedIndex) {
-              final screens = [
-                const HomeScreen(),
-                const Text('Search Screen'),
-                const Text('Chat Screen'),
-                const Text('Notification Screen'),
-                authState is Authenticated
-                    ? const ProfileScreen()
-                    : SignInScreen(),
-              ];
+              final screens = _buildScreens(authState);
               return Scaffold(
                 body: screens[selectedIndex],
                 bottomNavigationBar: NavigationWidget(
